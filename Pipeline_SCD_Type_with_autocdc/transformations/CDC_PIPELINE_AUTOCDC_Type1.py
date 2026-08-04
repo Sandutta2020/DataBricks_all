4f44e215cd54e2b9b8490f3af5a9c5b0c4a80d39
Sandutta2020 <Sandutta2020>
2026-08-04 11:02:32 +0000

    Restructuring CDC pipeline

---
+++ Pipeline_SCD_Type_with_autocdc/transformations/CDC_PIPELINE_AUTOCDC_Type1.py
@@ -0,0 +1,19 @@
+from pyspark import pipelines as dp
+from pyspark.sql.functions import col, expr
+
+@dp.view
+def users():
+  return spark.readStream.table("cdc_catalog.cdc_schema.customers_cdf")
+
+dp.create_streaming_table("cdc_catalog.cdc_schema.users_current")
+
+dp.create_auto_cdc_flow(
+  target = "cdc_catalog.cdc_schema.users_current",
+  source = "users",
+  keys = ["userId"],
+  sequence_by = col("_commit_timestamp"),
+  apply_as_deletes = expr("_change_type = 'DELETE'"),
+  apply_as_truncates = expr("_change_type = 'TRUNCATE'"),
+  except_column_list = ["_change_type", "_commit_version","_commit_timestamp"],
+  stored_as_scd_type = 1
+)

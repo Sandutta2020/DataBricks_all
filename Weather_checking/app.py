9ad89de5f6357973ac5a9aec6c375a5108257744
Sandutta2020 <Sandutta2020>
2026-08-15 19:39:03 +0000

    Checking order

--- Weather_checking/app.py
+++ Weather_checking/app.py
@@ -5,9 +5,12 @@ def main():
     order_path ='/Volumes/dbx_catalog/dbx_schema/orders_data/data_src'
 
     job =PySparkJob()
+    print("<<< PySparkJob object creation succeeded")
     print("<<< Loading and Cleaning Data >>>>")
     orders_df =job.load_and_clean_data(order_path)
-    orders_df.show(5,truncate=False)
+    sample_data = orders_df.limit(5).collect()
+    for row in sample_data:
+        print(f"Processing: {row}")
     job.stop()
 if __name__ == "__main__":
     main()
--- Weather_checking/pipeline.py
+++ Weather_checking/pipeline.py
@@ -5,9 +5,11 @@ from schema import Schemas
 
 class PySparkJob(PysparkJobInterface):
     def init_spark_session(self) -> SparkSession:
-        #return SparkSession.builder.appName("Orders_data_load").getOrCreate()
-        return DatabricksSession.builder.getOrCreate()
+        print("inside ")
+        return SparkSession.builder.appName("Orders_data_load").getOrCreate()
+        #return DatabricksSession.builder.getOrCreate()
     
     def load_and_clean_data(self,orders_path :str) -> DataFrame:
         orders_df = self.read_json(orders_path, schema=Schemas.orders)
+        orders_df.show()
         return orders_df
